7d1332ca704130fc1b0ba2d60b6682e55b01a378
gabrielvaz <gabriel.vaz22@gmail.com>
2022-05-12 13:00:01 -0300

    reading files

---
+++ src/contact.py
@@ -0,0 +1,6 @@
+class Contact:
+
+    def __init__(self, id, name, mail):
+        self.id = id
+        self.name = name
+        self.mail = mail
---
+++ src/main.py
@@ -0,0 +1,5 @@
+f_contacts = open('data/contacts.csv', encoding='latin_1')
+
+for line in f_contacts:
+    print(line, end='')
+
